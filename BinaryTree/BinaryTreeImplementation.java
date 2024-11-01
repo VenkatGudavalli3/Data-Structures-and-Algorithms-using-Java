bc5e85fc0fcd5e09f40bda46ea9b361eee7b2cbb
Venkat Gudavalli <venkatgudavalli379@gmail.com>
2024-11-01 19:54:33 +0530

    Pre Order Traversal

--- BinaryTree/BinaryTreeImplementation.java
+++ BinaryTree/BinaryTreeImplementation.java
@@ -1,5 +1,7 @@
 package BinaryTree;
 
+import java.util.Stack;
+
 public class BinaryTreeImplementation {
 
     private TreeNode root;
@@ -27,11 +29,44 @@ public class BinaryTreeImplementation {
         second.left = fourth;
         second.right = fifth;
     }
+    // pre order traversal
+    public void preOrderTraversal_Recursive(TreeNode root){
+        if(root==null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preOrderTraversal_Recursive(root.left);
+        preOrderTraversal_Recursive(root.right);
+    }
+
+    public void preOrderTraversal_Iterative(){
+        if(root == null){
+            return;
+        }
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+
+        while(!s.isEmpty()){
+            TreeNode temp = s.pop();
+            System.out.print(temp.data+" ");
+
+            if(temp.right!=null){
+                s.push(temp.right);
+            }
+            
+            if(temp.left!=null){
+                s.push(temp.left);
+            }
+        }
+
+    }
     
     public static void main(String[] args) {
         BinaryTreeImplementation b = new BinaryTreeImplementation();
 
         b.createBinaryTree();
+        //b.preOrderTraversal_Recursive(b.root);
+        b.preOrderTraversal_Iterative();
         
     }
 }
