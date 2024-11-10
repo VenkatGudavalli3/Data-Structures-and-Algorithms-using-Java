ec658cb7a1ea9bd8e5dc0e8347a9ad3215134ae6
Venkat Gudavalli <venkatgudavalli379@gmail.com>
2024-11-10 07:47:26 +0530

    Insertion in BST

--- BinaryTree/BinarySearchTree/BST_Implemntation.java
+++ BinaryTree/BinarySearchTree/BST_Implemntation.java
@@ -14,8 +14,46 @@ public class BST_Implemntation {
             this.data = data;
         }
     }
+
+    public void insert(int value){
+        root = insert(root,value);
+    }
+
+    public TreeNode insert(TreeNode root, int value){
+        if(root == null){
+            root = new TreeNode(value);
+            return root;
+        }
+        if(value<root.data){
+            root.left = insert(root.left, value);
+        }
+        else{
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+    public void inOrder(){
+        inOrder(root);
+    }
+    public void inOrder(TreeNode root){
+        if(root==null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
     public static void main(String[] args) {
         BST_Implemntation bst = new BST_Implemntation();
+        bst.insert(4);
+        bst.insert(5);
+        bst.insert(3);
+        bst.insert(1);
+        bst.insert(2);
+        bst.insert(6);
+
+        System.out.println("Binary Search Tree is: ");
+        bst.inOrder();
     }
     
 }
